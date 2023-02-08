0aeb2d8164d0bc0a4d9a4034467440f11f87deb8
Arman Radoš <armanrados@hotmail.com>
2023-02-08 22:20:30 +0100

    V1
    Vjezba 1

---
+++ eProdaja/eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodiService : IProizvodiService
+    {
+            List<Proizvodi> ProizvodiList =  new List<Proizvodi>(){new Proizvodi() { Id = 1, Naziv = "Laptop" }, new Proizvodi() { Id = 2, Naziv = "Mis" }};
+
+
+
+        public IEnumerable<Proizvodi> Get()
+        {
+            ProizvodiList.Add(new Proizvodi() { Id = 2, Naziv = "Tastatura" });
+            return ProizvodiList;
+        }
+
+       public Proizvodi GetById(int id)
+        {
+            return ProizvodiList.FirstOrDefault(x => x.Id == id);
+        }
+    }
+    }
+
